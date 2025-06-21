c5d5daa600157477df8f27f95d98b8ea53ad37cb
jamie 🌨️ <jamie@wisp.bio>
2025-06-21 17:52:41 -0400

    feat: mockup implementation

--- src/action.ts
+++ src/action.ts
@@ -1,8 +1,58 @@
+import c from 'chalk'
+import { Cause, Effect, Exit, Option } from 'effect'
+import { name, version } from '../package.json'
 import type { ProgramOptions } from './cli'
+import { log } from './lib/log'
+import { runCmd } from './lib/utils/run-cmd'
 
-export function handleAction(cmds: string[], options: ProgramOptions) {
-  console.log(cmds)
-  if (options.timeout) {
-    console.log('with timeout:', options.timeout)
-  }
+// const MAX_CMDS = 3
+
+function program(cmds: string[], options: ProgramOptions) {
+  return Effect.gen(function* () {
+    // if (cmds.length > MAX_CMDS) {
+    //   log.error(`too many commands: ${cmds.join(' ')}`)
+    //   process.exit(1)
+    // }
+
+    if (options.header) {
+      log.info(c.white.inverse(` ${name} v${version} `))
+
+      const cmdsList = cmds.map(cmd => `${c.white.bold.dim(cmd)}`).join(', ')
+      log.info(`> ${cmdsList}`)
+
+      log.break()
+    }
+
+    if (options.timeout) {
+      log.info(`with timeout: ${c.white.dim(options.timeout)}`)
+    }
+
+    yield* Effect.forEach(cmds, runCmd, {
+      concurrency: 'unbounded',
+    })
+
+    // cmd.on('close', (_, signal) => {
+    //   if (!signal) {
+    //     log.error('quitting...')
+    //     process.exit(1)
+    //   }
+    // })
+  })
+}
+
+export async function handleAction(cmds: string[], options: ProgramOptions) {
+  const exit = await program(cmds, options).pipe(Effect.runPromiseExit)
+
+  Exit.match(exit, {
+    onFailure: error => {
+      const cause = Cause.failureOption(error)
+
+      if (Option.isSome(cause)) {
+        log.error(cause.value.message)
+      }
+
+      process.exit(1)
+    },
+    onSuccess: () => {},
+  })
 }
