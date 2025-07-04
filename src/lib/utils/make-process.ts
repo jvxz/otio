b29059c6180317f32b89a9f898a8db61fa704a61
jamie 🌨️ <jamie@wisp.bio>
2025-07-03 21:46:38 -0400

    refactor: replace execa with child_process

--- src/lib/utils/make-process.ts
+++ src/lib/utils/make-process.ts
@@ -1,16 +1,23 @@
-import { execa } from 'execa'
+import child_process from 'node:child_process'
+import c from 'chalk'
 import { taskSignal } from '../abort'
+import { log } from '../log'
 
-export function makeProcess(cmd: string) {
-  const [command, ...args] = cmd.split(' ')
+export function makeProcess(rawCmd: string) {
+  const [command, ...args] = rawCmd.split(' ')
 
   if (!command) {
-    throw new Error(`command is empty: ${cmd}`)
+    throw new Error(`command is empty: ${rawCmd}`)
   }
 
-  const proc = execa(command, args, {
-    cancelSignal: taskSignal,
+  const proc = child_process.spawn(command, args, {
+    signal: taskSignal,
     stdio: 'inherit',
+    windowsHide: false,
+  })
+
+  proc.on('exit', code => {
+    log.warn(`${c.bold(rawCmd)} exited with code: ${code}`)
   })
 
   return proc
--- src/lib/utils/run-cmd.ts
+++ src/lib/utils/run-cmd.ts
@@ -1,5 +1,4 @@
 import { Data, Effect } from 'effect'
-import { ExecaError } from 'execa'
 import { makeProcess } from './make-process'
 
 class RunCmdError extends Data.TaggedError('RunCmdError')<{
@@ -11,28 +10,12 @@ export function runCmd(cmd: string) {
   return Effect.gen(function* () {
     const cmdProc = yield* Effect.try({
       catch: error => {
-        if (error instanceof ExecaError)
-          return new RunCmdError({
-            cause: error,
-            message: error.message,
-          })
-
         return new RunCmdError({
           cause: String(error),
           message: `unknown error occured when running command: ${cmd}`,
         })
       },
-      try: () => {
-        // create process as a promise
-        const proc = makeProcess(cmd)
-
-        // handle process exit
-        proc.on('close', code => {
-          process.exit(code ?? 1)
-        })
-
-        return proc
-      },
+      try: () => makeProcess(cmd),
     })
 
     return cmdProc
